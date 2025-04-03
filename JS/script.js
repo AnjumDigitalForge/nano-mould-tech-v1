3625090fd397777d41b74c08de77e02fad3829dd
AnjumDigitalForge <mnaveedanjum62@gmail.com>
2025-04-03 14:23:05 +0500

    1st version of Nano Mould Tech

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+//	prompt("Are You a Teacher!!!");
+//tooltip code JS
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
